ca6206bc9f7952b310b57b0001c4ea001b22d179
Pau Cuesta <pau.cuesta@gmail.com>
2021-04-28 23:45:03 +0200

    Pintando y colocando en posición

--- Assets/Scripts/CogerObjeto.cs
+++ Assets/Scripts/CogerObjeto.cs
@@ -28,6 +28,7 @@ public class CogerObjeto : MonoBehaviour
                 hasItem = true;
                 ObjectIwantToPickUp.transform.position = destino.transform.position; // sets the position of the object to your hand position
                 ObjectIwantToPickUp.transform.parent = destino.transform; //makes the object become a child of the parent so that it moves with the hands
+                ObjectIwantToPickUp.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
             }
         }
         if (Input.GetKeyDown("q") && hasItem == true) // if you have an item and get the key to remove the object, again can be any key
@@ -36,8 +37,9 @@ public class CogerObjeto : MonoBehaviour
             if (hasEncimera)
             {
                 Debug.Log("Suelta en encimers");
-                ObjectIwantToPickUp.transform.parent = null;
-                ObjectIwantToPickUp.transform.position = Encimera.transform.position; // make the object no be a child of the hands
+                ObjectIwantToPickUp.transform.parent = Encimera.transform;
+                ObjectIwantToPickUp.transform.position = Encimera.transform.GetChild(1).position; // make the object no be a child of the hands
+                Encimera.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
                 hasItem = false;
             }
         }
@@ -46,33 +48,31 @@ public class CogerObjeto : MonoBehaviour
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
         Debug.Log("Enter in collision");
-        if (other.gameObject.tag == "Utensilio") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if (other.gameObject.tag == "Utensilio" && !hasItem) //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             Debug.Log("Enter in utensilio");
             canpickup = true;  //set the pick up bool to true
             ObjectIwantToPickUp = other.gameObject; //set the gameobject you collided with to one you can reference
+            ObjectIwantToPickUp.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Sale de colisión");
-        canpickup = false; //when you leave the collider set the canpickup bool to false
-        // if (other.gameObject.tag == "Encimera")
-        // {
-        //     hasEncimera = false;
-        //     Encimera = null;
-        // }
+        canpickup = false;
+        ObjectIwantToPickUp.GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
     }
 
     private void OnCollisionEnter(Collision other) // to see when the player enters the collider
     {
         Debug.Log("Enter in collision");
-        if (other.gameObject.tag == "Encimera")
+        if (other.gameObject.tag == "Encimera" && !hasEncimeraAnObject(other.gameObject) && hasEncimera == false)
         {
             hasEncimera = true;
             Encimera = other.gameObject;
-            if (hasItem == true) {
-                Encimera.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().materials[0].color = new Color(1,1,1);
+            if (hasItem == true)
+            {
+                Encimera.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
             }
         }
     }
@@ -84,6 +84,19 @@ public class CogerObjeto : MonoBehaviour
         {
             hasEncimera = false;
             Encimera = null;
+            other.gameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
         }
+    }
+
+    private bool hasEncimeraAnObject(GameObject encimera)
+    {
+        foreach (Transform hijo in encimera.transform)
+        {
+            if (hijo.tag == "Utensilio")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
