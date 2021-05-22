f8350ddf73fa2949216983961a43e95c6c88cf65
Pau Cuesta <pau.cuesta@gmail.com>
2021-05-22 21:03:30 +0200

    Teclas especiales cambio de nivel

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+            SceneManager.LoadScene("Nivel2");
+        if (Input.GetKeyDown("1"))
+            SceneManager.LoadScene("Nivel1");
+        if (Input.GetKeyDown("2"))
+            SceneManager.LoadScene("Nivel2");
+        if (Input.GetKeyDown("3"))
+            SceneManager.LoadScene("Nivel3");
+        if (Input.GetKeyDown("4"))
+            SceneManager.LoadScene("Nivel4");
+        if (Input.GetKeyDown("5"))
+            SceneManager.LoadScene("Nivel5");
+    }
+}
