509b05820cf747da330bf00405bda6aae00a40fe
Pau Cuesta <pau.cuesta@gmail.com>
2021-05-27 23:01:33 +0200

    Humo movimiento

--- Assets/Scripts/Chef/moveChef.cs
+++ Assets/Scripts/Chef/moveChef.cs
@@ -11,38 +11,38 @@ public class moveChef : MonoBehaviour
     public float rotationSpeed = 300.0f;
     public GameObject menuSuperior;
     public Camera camara;
+    public GameObject humo;
 
     private Rigidbody body;
 
-    public Vector3 vectorPlayer;
+    public Vector3 vectorPlayer, lastVector;
     public float lastPosX, lastPosY, posX, posY;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
-        lastPosX = Input.mousePosition.x;
-        lastPosY = Input.mousePosition.y;
-        posX = posY = 0;
-
+        vectorPlayer = lastVector = new Vector3(1, -1.0f, 1);
     }
 
     void Update()
     {
-        if (menuSuperior.GetComponent<Timer>().getTime() > 0) {
-            if (GlobalVariables.mouse) {
-                //if (Input.mousePosition.x - lastPosX != 0) posX = (Input.mousePosition.x - lastPosX) * 2.0f;
-                //if (Input.mousePosition.y - lastPosY != 0) posY = (Input.mousePosition.y - lastPosY) * 2.0f;
-                //lastPosX = Input.mousePosition.x;
-                //lastPosY = Input.mousePosition.y;
+        if (menuSuperior.GetComponent<Timer>().getTime() > 0)
+        {
+            if (GlobalVariables.mouse)
+            {
                 Vector3 dir = Input.mousePosition - camara.WorldToScreenPoint(transform.position);
                 vectorPlayer = new Vector3(dir.x, -1.0f, dir.y);
             }
-            else {
+            else
+            {
                 vectorPlayer = new Vector3(Input.GetAxis("Horizontal") * 10f, -1.0f, Input.GetAxis("Vertical") * 10f);
                 transform.LookAt(transform.position + new Vector3(vectorPlayer.x, 0, vectorPlayer.z));
-                
             }
+        }
+        if (vectorPlayer != lastVector)
+        {
+            Instantiate(humo, transform.position + new Vector3(-0.1f, 0, 0), transform.rotation);
         }
     }
 
